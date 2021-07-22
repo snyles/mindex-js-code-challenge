b6153ce0ad897d8cfcd36fffc2e6ce97d19269b1
snyles <sniles@gmail.com>
2021-07-22 12:15:38 -0400

    display both direct and indirect reports

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -13,6 +13,7 @@ export class EmployeeComponent implements OnInit {
   @Input() employee: Employee;
   // @Input() employees: Employee[];
   directReports: Employee[] = [];
+  totalReports: number = 0;
 
   constructor(private employeeService: EmployeeService) {
   }
@@ -20,10 +21,15 @@ export class EmployeeComponent implements OnInit {
   ngOnInit(): void {
 
     // if employee has direct results, populate array with employee data
+    // and add indirect reports to reports total
     if(this.employee.directReports) {
+      this.totalReports = this.employee.directReports.length
       const direct = []
       this.employee.directReports.forEach((id) => {
-        this.employeeService.get(id).subscribe(emp => direct.push(emp));
+        this.employeeService.get(id).subscribe(emp => {
+          this.totalReports += emp.directReports ? emp.directReports.length : 0;
+          direct.push(emp)
+        });
       })
       this.directReports = direct;
     }
